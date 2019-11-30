9869ecf5b9b7c46cee666ac8b0c01abc34452199
muneblog <muneblog0321@gmail.com>
2019-11-30 14:17:25 +0900

    slackbotを使う

--- ex01.rb
+++ ex01.rb
@@ -1,3 +1,12 @@
 class Wakaba
-  
+  def initialize
+    @magic_item = "魔法の杖"
+  end
+
+  def self_introduce
+    puts "私のアイテムは" + @magic_item + "です"
+  end
 end
+
+wakaba = Wakaba.new
+wakaba.self_introduce
